125217fa4044a55812ef5e4217bb0d8e9cf6e7d2
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2023-06-10 22:50:27 -0700

    added User properties

--- QuantumLearn/Areas/Identity/Data/ApplicationUser.cs
+++ QuantumLearn/Areas/Identity/Data/ApplicationUser.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Microsoft.AspNetCore.Identity;
 
 namespace QuantumLearn.Areas.Identity.Data;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
-public class ApplicationUser : IdentityUser
+public class ApplicationUser : IdentityUser  // Ctrl+click to see what's built-in to IdentityUser class; it has built-in ID, username, email, phone, etc.
 {
-}
+    public string FirstName { get; set; }
 
+    public string LastName { get; set; }
+
+    public byte[]? ImageDataForUser { get; set; }  // not required; ? allows for null value
+}
