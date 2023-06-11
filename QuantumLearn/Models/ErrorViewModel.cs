f9c7aba78326fdb21b628b81af196bcf2a6816fe
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2023-06-10 21:17:47 -0700

    Add project files.

---
+++ QuantumLearn/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuantumLearn.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
