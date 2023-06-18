9f9c40649d93913bdf71b91304b7dc3280bb83be
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2023-06-17 23:14:33 -0700

    added quiz functionality and 13 views for 13 quizzes

---
+++ QuantumLearn/Controllers/QuizController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuantumLearn.Controllers
+{
+    public class QuizController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index", "Lesson");
+        }
+
+        public IActionResult Quiz1()
+        {
+            return View();
+        }
+
+        public IActionResult Quiz2()
+        {
+            return View();
+        }
+
+        public IActionResult Quiz3()
+        {
+            return View();
+        }
+
+        public IActionResult Quiz4()
+        {
+            return View();
+        }
+
+        public IActionResult Quiz5()
+        {
+            return View();
+        }
+
+        public IActionResult Quiz6()
+        {
+            return View();
+        }
+
+        public IActionResult Quiz7()
+        {
+            return View();
+        }
+
+        public IActionResult Quiz8()
+        {
+            return View();
+        }
+
+        public IActionResult Quiz9()
+        {
+            return View();
+        }
+
+        public IActionResult Quiz10()
+        {
+            return View();
+        }
+
+        public IActionResult Quiz11()
+        {
+            return View();
+        }
+
+        public IActionResult Quiz12()
+        {
+            return View();
+        }
+
+        public IActionResult Quiz13()
+        {
+            return View();
+        }
+    }
+}
