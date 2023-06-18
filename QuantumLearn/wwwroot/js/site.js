cec93c11efccf7c810d69ad56fa1c9ecd8f2742e
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2023-06-18 16:59:26 -0700

    added pop-up to give answers to quiz questions

--- QuantumLearn/wwwroot/js/site.js
+++ QuantumLearn/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function correct() {
+    alert("Correct!");
+}
+
+function incorrect() {
+    alert("Incorrect");
+}
