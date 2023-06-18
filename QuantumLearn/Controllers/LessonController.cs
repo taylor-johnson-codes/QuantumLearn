16b46d624a5ff59eb698accf7dee76111e91f678
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2023-06-17 20:43:57 -0700

    added Lesson controller and view

---
+++ QuantumLearn/Controllers/LessonController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuantumLearn.Controllers
+{
+    public class LessonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Num1()
+        {
+            return View();
+        }
+
+        public IActionResult Num2()
+        {
+            return View();
+        }
+
+        public IActionResult Num3()
+        {
+            return View();
+        }
+
+        public IActionResult Num4()
+        {
+            return View();
+        }
+
+        public IActionResult Num5()
+        {
+            return View();
+        }
+
+        public IActionResult Num6()
+        {
+            return View();
+        }
+
+        public IActionResult Num7()
+        {
+            return View();
+        }
+
+        public IActionResult Num8()
+        {
+            return View();
+        }
+
+        public IActionResult Num9()
+        {
+            return View();
+        }
+
+        public IActionResult Num10()
+        {
+            return View();
+        }
+
+        public IActionResult Num11()
+        {
+            return View();
+        }
+
+        public IActionResult Num12()
+        {
+            return View();
+        }
+
+        public IActionResult Num13()
+        {
+            return View();
+        }
+    }
+}
