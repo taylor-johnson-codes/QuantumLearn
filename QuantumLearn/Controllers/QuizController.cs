75642774c187cff63d9e08b66f357d90185ecb86
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2023-07-08 21:10:22 -0700

    deleted comment

--- QuantumLearn/Controllers/QuizController.cs
+++ QuantumLearn/Controllers/QuizController.cs
@@ -114,47 +114,5 @@ namespace QuantumLearn.Controllers
         {
             return View();
         }
-
-        // ------------------------------------------------------------------------------------ //
-
-        // All code below was copy/pasted from ChatGPT:
-        /*
-        private ApplicationDbContext _context;
-
-        public QuizController()
-        {
-            _context = new ApplicationDbContext();
-        }
-
-        // GET: Quiz
-        public ActionResult Index()
-        {
-            var questions = _context.Questions.Include(q => q.Answers).ToList();
-            return View(questions);
-        }
-
-        [HttpPost]
-        public ActionResult SubmitQuiz(IEnumerable<QuizResult> results)
-        {
-            string currentUserId = User.Identity.GetUserId();
-
-            foreach (var result in results)
-            {
-                result.UserId = currentUserId;
-                _context.QuizResults.Add(result);
-            }
-
-            _context.SaveChanges();
-
-            return RedirectToAction("Results");
-        }
-
-        public ActionResult Results()
-        {
-            string currentUserId = User.Identity.GetUserId();
-            var results = _context.QuizResults.Where(r => r.UserId == currentUserId).Include(r => r.Question).Include(r => r.Answer).ToList();
-            return View(results);
-        }
-        */
     }
 }
