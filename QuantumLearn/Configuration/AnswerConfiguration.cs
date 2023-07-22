587bc7e1526ee4919b55accbcc595bef5c6ce056
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2023-07-22 12:02:35 -0700

    added Quiz 2 seed data

--- QuantumLearn/Configuration/AnswerConfiguration.cs
+++ QuantumLearn/Configuration/AnswerConfiguration.cs
@@ -2,9 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using QuantumLearn.Models;
 
-// The Answer table with properties is already in the database; this is to add seed data to it.
-// Resource used: https://code-maze.com/migrations-and-seed-data-efcore/
-
 namespace QuantumLearn.Configuration
 {
     public class AnswerConfiguration : IEntityTypeConfiguration<Answer>
@@ -19,20 +16,35 @@ namespace QuantumLearn.Configuration
                 // Quiz 1 Question 2
                 new Answer { Id = 4, QuizNum = 1, QuestionId = 2, Text = "1990s - 1999", IsCorrect = false, OptionNum = 1 },
                 new Answer { Id = 5, QuizNum = 1, QuestionId = 2, Text = "2000s - 2009", IsCorrect = false, OptionNum = 2 },
-                new Answer { Id = 6, QuizNum = 1, QuestionId = 2, Text = "2010s - Present", IsCorrect = true, OptionNum = 3 }
+                new Answer { Id = 6, QuizNum = 1, QuestionId = 2, Text = "2010s - Present", IsCorrect = true, OptionNum = 3 },
 
-                // Quiz 2
-                // Quiz 3
-                // Quiz 4
-                // Quiz 5
-                // Quiz 6
-                // Quiz 7
-                // Quiz 8
-                // Quiz 9
-                // Quiz 10
-                // Quiz 11
-                // Quiz 12
-                // Quiz 13
+                // Quiz 2 Question 1
+                new Answer { Id = 7, QuizNum = 2, QuestionId = 3, Text = "True", IsCorrect = false, OptionNum = 1 },
+                new Answer { Id = 8, QuizNum = 2, QuestionId = 3, Text = "False", IsCorrect = true, OptionNum = 2 },
+                // Quiz 2 Question 2
+                new Answer { Id = 9, QuizNum = 2, QuestionId = 4, Text = "True", IsCorrect = true, OptionNum = 1 },
+                new Answer { Id = 10, QuizNum = 2, QuestionId = 4, Text = "False", IsCorrect = false, OptionNum = 2 },
+                // Quiz 2 Question 3
+                new Answer { Id = 11, QuizNum = 2, QuestionId = 5, Text = "True", IsCorrect = false, OptionNum = 1 },
+                new Answer { Id = 12, QuizNum = 2, QuestionId = 5, Text = "False", IsCorrect = true, OptionNum = 2 },
+                // Quiz 2 Question 4
+                new Answer { Id = 13, QuizNum = 2, QuestionId = 6, Text = "True", IsCorrect = false, OptionNum = 1 },
+                new Answer { Id = 14, QuizNum = 2, QuestionId = 6, Text = "False", IsCorrect = true, OptionNum = 2 },
+                // Quiz 2 Question 5
+                new Answer { Id = 15, QuizNum = 2, QuestionId = 7, Text = "True", IsCorrect = true, OptionNum = 1 },
+                new Answer { Id = 16, QuizNum = 2, QuestionId = 7, Text = "False", IsCorrect = false, OptionNum = 2 }
+
+            // Quiz 3
+            // Quiz 4
+            // Quiz 5
+            // Quiz 6
+            // Quiz 7
+            // Quiz 8
+            // Quiz 9
+            // Quiz 10
+            // Quiz 11
+            // Quiz 12
+            // Quiz 13
             );
         }
     }
