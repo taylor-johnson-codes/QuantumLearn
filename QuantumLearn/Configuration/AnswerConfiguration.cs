6f66a46ecfee44305dae49065ff82737a9a8a9ca
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2023-07-22 17:25:14 -0700

    added seed data for Quiz 3 and 4

--- QuantumLearn/Configuration/AnswerConfiguration.cs
+++ QuantumLearn/Configuration/AnswerConfiguration.cs
@@ -32,11 +32,49 @@ namespace QuantumLearn.Configuration
                 new Answer { Id = 14, QuizNum = 2, QuestionId = 6, Text = "False", IsCorrect = true, OptionNum = 2 },
                 // Quiz 2 Question 5
                 new Answer { Id = 15, QuizNum = 2, QuestionId = 7, Text = "True", IsCorrect = true, OptionNum = 1 },
-                new Answer { Id = 16, QuizNum = 2, QuestionId = 7, Text = "False", IsCorrect = false, OptionNum = 2 }
+                new Answer { Id = 16, QuizNum = 2, QuestionId = 7, Text = "False", IsCorrect = false, OptionNum = 2 },
 
-            // Quiz 3
-            // Quiz 4
-            // Quiz 5
+                // Quiz 3 Question 1
+                new Answer { Id = 17, QuizNum = 3, QuestionId = 8, Text = "True", IsCorrect = true, OptionNum = 1 },
+                new Answer { Id = 18, QuizNum = 3, QuestionId = 8, Text = "False", IsCorrect = false, OptionNum = 2 },
+                // Quiz 3 Question 2
+                new Answer { Id = 19, QuizNum = 3, QuestionId = 9, Text = "True", IsCorrect = false, OptionNum = 1 },
+                new Answer { Id = 20, QuizNum = 3, QuestionId = 9, Text = "False", IsCorrect = true, OptionNum = 2 },
+                // Quiz 3 Question 3
+                new Answer { Id = 21, QuizNum = 3, QuestionId = 10, Text = "It can help in the creation of quantum-resistant cryptographic methods.", IsCorrect = true, OptionNum = 1 },
+                new Answer { Id = 22, QuizNum = 3, QuestionId = 10, Text = "It can assist in generating more complex passwords.", IsCorrect = false, OptionNum = 2 },
+                new Answer { Id = 23, QuizNum = 3, QuestionId = 10, Text = "It can help in understanding classical computers better.", IsCorrect = false, OptionNum = 3 },
+                // Quiz 3 Question 4
+                new Answer { Id = 24, QuizNum = 3, QuestionId = 11, Text = "Physics, computer science, mathematics, and information theory", IsCorrect = true, OptionNum = 1 },
+                new Answer { Id = 25, QuizNum = 3, QuestionId = 11, Text = "Literature, music, art, and culinary", IsCorrect = false, OptionNum = 2 },
+                new Answer { Id = 26, QuizNum = 3, QuestionId = 11, Text = "Politics, sociology, and philosophy", IsCorrect = false, OptionNum = 3 },
+
+                // Quiz 4 Question 1
+                new Answer { Id = 27, QuizNum = 4, QuestionId = 12, Text = "True", IsCorrect = false, OptionNum = 1 },
+                new Answer { Id = 28, QuizNum = 4, QuestionId = 12, Text = "False", IsCorrect = true, OptionNum = 2 },
+                // Quiz 4 Question 2
+                new Answer { Id = 29, QuizNum = 4, QuestionId = 13, Text = "True", IsCorrect = true, OptionNum = 1 },
+                new Answer { Id = 30, QuizNum = 4, QuestionId = 13, Text = "False", IsCorrect = false, OptionNum = 2 },
+                // Quiz 4 Question 3
+                new Answer { Id = 31, QuizNum = 4, QuestionId = 14, Text = "True", IsCorrect = false, OptionNum = 1 },
+                new Answer { Id = 32, QuizNum = 4, QuestionId = 14, Text = "False", IsCorrect = true, OptionNum = 2 },
+                // Quiz 4 Question 4
+                new Answer { Id = 33, QuizNum = 4, QuestionId = 15, Text = "Quality bit", IsCorrect = false, OptionNum = 1 },
+                new Answer { Id = 34, QuizNum = 4, QuestionId = 15, Text = "Quantum bit", IsCorrect = true, OptionNum = 2 },
+                new Answer { Id = 35, QuizNum = 4, QuestionId = 15, Text = "Quick bit", IsCorrect = false, OptionNum = 3 },
+                // Quiz 4 Question 5
+                new Answer { Id = 36, QuizNum = 4, QuestionId = 16, Text = "The state of one qubit is completely unrelated to the state of another.", IsCorrect = false, OptionNum = 1 },
+                new Answer { Id = 37, QuizNum = 4, QuestionId = 16, Text = "The state of one qubit is directly related to the state of another, no matter how far apart they are.", IsCorrect = true, OptionNum = 2 },
+                new Answer { Id = 38, QuizNum = 4, QuestionId = 16, Text = "The qubits are physically tangled with each other.", IsCorrect = false, OptionNum = 3 },
+                // Quiz 4 Question 6
+                new Answer { Id = 39, QuizNum = 4, QuestionId = 17, Text = "Superposition and entanglement", IsCorrect = true, OptionNum = 1 },
+                new Answer { Id = 40, QuizNum = 4, QuestionId = 17, Text = "Superposition and division", IsCorrect = false, OptionNum = 2 },
+                new Answer { Id = 41, QuizNum = 4, QuestionId = 17, Text = "Entanglement and multiplication", IsCorrect = false, OptionNum = 3 }
+
+                // Quiz 5
+
+
+
             // Quiz 6
             // Quiz 7
             // Quiz 8
--- QuantumLearn/Configuration/QuestionConfiguration.cs
+++ QuantumLearn/Configuration/QuestionConfiguration.cs
@@ -18,10 +18,22 @@ namespace QuantumLearn.Configuration
                 new Question { Id = 4, QuizNum = 2, Text = "Qubits, unlike bits, can be processed in parallel due to their ability to be in multiple states at once." },
                 new Question { Id = 5, QuizNum = 2, Text = "In a classical computer, each bit is dependent on the others, a concept referred to as entanglement." },
                 new Question { Id = 6, QuizNum = 2, Text = "Computations on a classical computer are probabilistic, which means the output can vary for a given input and a given set of instructions." },
-                new Question { Id = 7, QuizNum = 2, Text = "Quantum computers can potentially handle much larger amounts of data than classical computers because a qubit can hold an infinite number of possible values." }
+                new Question { Id = 7, QuizNum = 2, Text = "Quantum computers can potentially handle much larger amounts of data than classical computers because a qubit can hold an infinite number of possible values." },
 
                 // Quiz 3
+                new Question { Id = 8, QuizNum = 3, Text = "Quantum computing provides many opportunities for contributing to new discoveries and innovations." },
+                new Question { Id = 9, QuizNum = 3, Text = "Quantum computing doesn't have the potential to impact the field of machine learning." },
+                new Question { Id = 10, QuizNum = 3, Text = "Why is understanding quantum computing crucial in the context of cryptography?" },
+                new Question { Id = 11, QuizNum = 3, Text = "Quantum computing lies at the intersection of which fields?" },
+
                 // Quiz 4
+                new Question { Id = 12, QuizNum = 4, Text = "A qubit can be in one of two states: 0 or 1." },
+                new Question { Id = 13, QuizNum = 4, Text = "Qubits can be in a state of 0, 1, or both at the same time due to a property in quantum mechanics known as superposition." },
+                new Question { Id = 14, QuizNum = 4, Text = "In the quantum world, the exact state of a qubit can be known before measuring it." },
+                new Question { Id = 15, QuizNum = 4, Text = "The term \"qubit\" comes from which phrase?" },
+                new Question { Id = 16, QuizNum = 4, Text = "What does it mean when qubits are said to be entangled?" },
+                new Question { Id = 17, QuizNum = 4, Text = "Which properties of qubits enable quantum computers to process a massive number of possibilities at once?" }
+
                 // Quiz 5
                 // Quiz 6
                 // Quiz 7
