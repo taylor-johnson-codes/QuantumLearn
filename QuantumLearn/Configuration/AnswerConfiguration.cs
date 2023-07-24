22da827b0e4f7311e2e3ad9324ab223c490c467c
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2023-07-24 12:56:36 -0700

    added seed data for Quizzes 5-7

--- QuantumLearn/Configuration/AnswerConfiguration.cs
+++ QuantumLearn/Configuration/AnswerConfiguration.cs
@@ -41,9 +41,9 @@ namespace QuantumLearn.Configuration
                 new Answer { Id = 19, QuizNum = 3, QuestionId = 9, Text = "True", IsCorrect = false, OptionNum = 1 },
                 new Answer { Id = 20, QuizNum = 3, QuestionId = 9, Text = "False", IsCorrect = true, OptionNum = 2 },
                 // Quiz 3 Question 3
-                new Answer { Id = 21, QuizNum = 3, QuestionId = 10, Text = "It can help in the creation of quantum-resistant cryptographic methods.", IsCorrect = true, OptionNum = 1 },
-                new Answer { Id = 22, QuizNum = 3, QuestionId = 10, Text = "It can assist in generating more complex passwords.", IsCorrect = false, OptionNum = 2 },
-                new Answer { Id = 23, QuizNum = 3, QuestionId = 10, Text = "It can help in understanding classical computers better.", IsCorrect = false, OptionNum = 3 },
+                new Answer { Id = 21, QuizNum = 3, QuestionId = 10, Text = "It can help in the creation of quantum-resistant cryptographic methods", IsCorrect = true, OptionNum = 1 },
+                new Answer { Id = 22, QuizNum = 3, QuestionId = 10, Text = "It can assist in generating more complex passwords", IsCorrect = false, OptionNum = 2 },
+                new Answer { Id = 23, QuizNum = 3, QuestionId = 10, Text = "It can help in understanding classical computers better", IsCorrect = false, OptionNum = 3 },
                 // Quiz 3 Question 4
                 new Answer { Id = 24, QuizNum = 3, QuestionId = 11, Text = "Physics, computer science, mathematics, and information theory", IsCorrect = true, OptionNum = 1 },
                 new Answer { Id = 25, QuizNum = 3, QuestionId = 11, Text = "Literature, music, art, and culinary", IsCorrect = false, OptionNum = 2 },
@@ -63,26 +63,72 @@ namespace QuantumLearn.Configuration
                 new Answer { Id = 34, QuizNum = 4, QuestionId = 15, Text = "Quantum bit", IsCorrect = true, OptionNum = 2 },
                 new Answer { Id = 35, QuizNum = 4, QuestionId = 15, Text = "Quick bit", IsCorrect = false, OptionNum = 3 },
                 // Quiz 4 Question 5
-                new Answer { Id = 36, QuizNum = 4, QuestionId = 16, Text = "The state of one qubit is completely unrelated to the state of another.", IsCorrect = false, OptionNum = 1 },
-                new Answer { Id = 37, QuizNum = 4, QuestionId = 16, Text = "The state of one qubit is directly related to the state of another, no matter how far apart they are.", IsCorrect = true, OptionNum = 2 },
-                new Answer { Id = 38, QuizNum = 4, QuestionId = 16, Text = "The qubits are physically tangled with each other.", IsCorrect = false, OptionNum = 3 },
+                new Answer { Id = 36, QuizNum = 4, QuestionId = 16, Text = "The state of one qubit is completely unrelated to the state of another", IsCorrect = false, OptionNum = 1 },
+                new Answer { Id = 37, QuizNum = 4, QuestionId = 16, Text = "The state of one qubit is directly related to the state of another, no matter how far apart they are", IsCorrect = true, OptionNum = 2 },
+                new Answer { Id = 38, QuizNum = 4, QuestionId = 16, Text = "The qubits are physically tangled with each other", IsCorrect = false, OptionNum = 3 },
                 // Quiz 4 Question 6
                 new Answer { Id = 39, QuizNum = 4, QuestionId = 17, Text = "Superposition and entanglement", IsCorrect = true, OptionNum = 1 },
                 new Answer { Id = 40, QuizNum = 4, QuestionId = 17, Text = "Superposition and division", IsCorrect = false, OptionNum = 2 },
-                new Answer { Id = 41, QuizNum = 4, QuestionId = 17, Text = "Entanglement and multiplication", IsCorrect = false, OptionNum = 3 }
+                new Answer { Id = 41, QuizNum = 4, QuestionId = 17, Text = "Entanglement and multiplication", IsCorrect = false, OptionNum = 3 },
 
-                // Quiz 5
+                // Quiz 5 Question 1
+                new Answer { Id = 42, QuizNum = 5, QuestionId = 18, Text = "True", IsCorrect = true, OptionNum =  1 },
+                new Answer { Id = 43, QuizNum = 5, QuestionId = 18, Text = "False", IsCorrect = false, OptionNum =  2 },
+                // Quiz 5 Question 2
+                new Answer { Id = 44, QuizNum = 5, QuestionId = 19, Text = "True", IsCorrect = false, OptionNum =  1 },
+                new Answer { Id = 45, QuizNum = 5, QuestionId = 19, Text = "False", IsCorrect = true, OptionNum =  2 },
+                // Quiz 5 Question 3
+                new Answer { Id = 46, QuizNum = 5, QuestionId = 20, Text = "True", IsCorrect = true, OptionNum =  1 },
+                new Answer { Id = 47, QuizNum = 5, QuestionId = 20, Text = "False", IsCorrect = false, OptionNum =  2 },
+                // Quiz 5 Question 4
+                new Answer { Id = 48, QuizNum = 5, QuestionId = 21, Text = "Sports and entertainment", IsCorrect = false, OptionNum =  1 },
+                new Answer { Id = 49, QuizNum = 5, QuestionId = 21, Text = "Physics and engineering", IsCorrect = true, OptionNum =  2 },
+                new Answer { Id = 50, QuizNum = 5, QuestionId = 21, Text = "History and geography", IsCorrect = false, OptionNum =  3 },
+                // Quiz 5 Question 5
+                new Answer { Id = 51, QuizNum = 5, QuestionId = 22, Text = "They are used to describe the state of qubits", IsCorrect = true, OptionNum =  1 },
+                new Answer { Id = 52, QuizNum = 5, QuestionId = 22, Text = "They are used to power the quantum computer", IsCorrect = false, OptionNum =  2 },
+                new Answer { Id = 53, QuizNum = 5, QuestionId = 22, Text = "They are used to construct the physical hardware of a quantum computer", IsCorrect = false, OptionNum =  3 },
+                // Quiz 5 Question 6
+                new Answer { Id = 54, QuizNum = 5, QuestionId = 23, Text = "A real part and a simple part", IsCorrect = false, OptionNum =  1 },
+                new Answer { Id = 55, QuizNum = 5, QuestionId = 23, Text = "A real part and an imaginary part", IsCorrect = true, OptionNum =  2 },
+                new Answer { Id = 56, QuizNum = 5, QuestionId = 23, Text = "An imaginary part and a multiple part", IsCorrect = false, OptionNum =  3 },
 
+                // Quiz 6 Question 1
+                new Answer { Id = 57, QuizNum = 6, QuestionId = 24, Text = "True", IsCorrect = false, OptionNum =  1 },
+                new Answer { Id = 58, QuizNum = 6, QuestionId = 24, Text = "False", IsCorrect = true, OptionNum =  2 },
+                // Quiz 6 Question 2
+                new Answer { Id = 59, QuizNum = 6, QuestionId = 25, Text = "True", IsCorrect = true, OptionNum =  1 },
+                new Answer { Id = 60, QuizNum = 6, QuestionId = 25, Text = "False", IsCorrect = false, OptionNum =  2 },
+                // Quiz 6 Question 3
+                new Answer { Id = 61, QuizNum = 6, QuestionId = 26, Text = "It retains its superposition state", IsCorrect = false, OptionNum =  1 },
+                new Answer { Id = 62, QuizNum = 6, QuestionId = 26, Text = "It forgets about its superposition state and acts like a regular bit until unobserved again", IsCorrect = true, OptionNum =  2 },
+                new Answer { Id = 63, QuizNum = 6, QuestionId = 26, Text = "It permanently remains in the chosen state", IsCorrect = false, OptionNum =  3 },
 
+                // Quiz 7 Question 1
+                new Answer { Id = 64, QuizNum = 7, QuestionId = 27, Text = "True", IsCorrect = false, OptionNum = 1 },
+                new Answer { Id = 65, QuizNum = 7, QuestionId = 27, Text = "False", IsCorrect = true, OptionNum = 2 },
+                // Quiz 7 Question 2
+                new Answer { Id = 66, QuizNum = 7, QuestionId = 28, Text = "True", IsCorrect = false, OptionNum = 1 },
+                new Answer { Id = 67, QuizNum = 7, QuestionId = 28, Text = "False", IsCorrect = true, OptionNum = 2 },
+                // Quiz 7 Question 3
+                new Answer { Id = 68, QuizNum = 7, QuestionId = 29, Text = "Superposition", IsCorrect = true, OptionNum = 1 },
+                new Answer { Id = 69, QuizNum = 7, QuestionId = 29, Text = "Either heads or tails", IsCorrect = false, OptionNum = 2 },
+                new Answer { Id = 70, QuizNum = 7, QuestionId = 29, Text = "Neither heads nor tails", IsCorrect = false, OptionNum = 3 },
+                // Quiz 7 Question 4
+                new Answer { Id = 71, QuizNum = 7, QuestionId = 30, Text = "Can be predetermined", IsCorrect = false, OptionNum = 1 },
+                new Answer { Id = 72, QuizNum = 7, QuestionId = 30, Text = "Is always heads", IsCorrect = false, OptionNum = 2 },
+                new Answer { Id = 73, QuizNum = 7, QuestionId = 30, Text = "Is probabilistic, depending on α and β", IsCorrect = true, OptionNum = 3 },
+                // Quiz 7 Question 5
+                new Answer { Id = 74, QuizNum = 7, QuestionId = 31, Text = "The chances of finding a qubit in a particular state when we measure it", IsCorrect = true, OptionNum = 1 },
+                new Answer { Id = 75, QuizNum = 7, QuestionId = 31, Text = "Determining the state of the qubit before measurement", IsCorrect = false, OptionNum = 2 },
+                new Answer { Id = 76, QuizNum = 7, QuestionId = 31, Text = "Changing the state of a qubit after measurement", IsCorrect = false, OptionNum = 3 }
 
-            // Quiz 6
-            // Quiz 7
-            // Quiz 8
-            // Quiz 9
-            // Quiz 10
-            // Quiz 11
-            // Quiz 12
-            // Quiz 13
+                // Quiz 8 Question 
+                // Quiz 9 Question 
+                // Quiz 10 Question 
+                // Quiz 11 Question 
+                // Quiz 12 Question 
+                // Quiz 13 Question 
             );
         }
     }
--- QuantumLearn/Configuration/QuestionConfiguration.cs
+++ QuantumLearn/Configuration/QuestionConfiguration.cs
@@ -32,17 +32,47 @@ namespace QuantumLearn.Configuration
                 new Question { Id = 14, QuizNum = 4, Text = "In the quantum world, the exact state of a qubit can be known before measuring it." },
                 new Question { Id = 15, QuizNum = 4, Text = "The term \"qubit\" comes from which phrase?" },
                 new Question { Id = 16, QuizNum = 4, Text = "What does it mean when qubits are said to be entangled?" },
-                new Question { Id = 17, QuizNum = 4, Text = "Which properties of qubits enable quantum computers to process a massive number of possibilities at once?" }
+                new Question { Id = 17, QuizNum = 4, Text = "Which properties of qubits enable quantum computers to process a massive number of possibilities at once?" },
 
                 // Quiz 5
+                new Question { Id = 18, QuizNum = 5, Text = "The imaginary unit, denoted as \"i\", is defined as the square root of -1." },
+                new Question { Id = 19, QuizNum = 5, Text = "When combined with real numbers, imaginary numbers form what are known as simple numbers." },
+                new Question { Id = 20, QuizNum = 5, Text = "Quantum gates perform operations on qubits and are described using matrices that often involve complex numbers." },
+                new Question { Id = 21, QuizNum = 5, Text = "The imaginary unit is widely used in:" },
+                new Question { Id = 22, QuizNum = 5, Text = "In quantum computing, why are complex numbers and the imaginary unit essential?" },
+                new Question { Id = 23, QuizNum = 5, Text = "What are complex numbers made up of?" },
+
                 // Quiz 6
+                new Question { Id = 24, QuizNum = 6, Text = "Until a qubit is measured, its exact state can be known." },
+                new Question { Id = 25, QuizNum = 6, Text = "A qubit can be in a state where it's both heads and tails at the same time." },
+                new Question { Id = 26, QuizNum = 6, Text = "What happens to a qubit after it's measured and \"chooses\" a state?" },
+
                 // Quiz 7
+                new Question { Id = 27, QuizNum = 7, Text = "The \"amount\" of α and β in a qubit's state does not affect the chances of seeing heads or tails when it is measured." },
+                new Question { Id = 28, QuizNum = 7, Text = "Before a qubit is measured, it chooses to be in a definite state of either heads or tails." },
+                new Question { Id = 29, QuizNum = 7, Text = "When a qubit is not being measured, it is in a state of:" },
+                new Question { Id = 30, QuizNum = 7, Text = "The exact state of a qubit when we measure it:" },
+                new Question { Id = 31, QuizNum = 7, Text = "Probability in quantum computing is about:" }
+
+                /*
                 // Quiz 8
+                new Question { Id = , QuizNum = , Text = "" },
+                
                 // Quiz 9
+                new Question { Id = , QuizNum = , Text = "" },
+                
                 // Quiz 10
+                new Question { Id = , QuizNum = , Text = "" },
+
                 // Quiz 11
+                new Question { Id = , QuizNum = , Text = "" },
+
                 // Quiz 12
+                new Question { Id = , QuizNum = , Text = "" },
+
                 // Quiz 13
+                new Question { Id = , QuizNum = , Text = "" },
+                */
             );
         }
     }
